205347403316f066f394e0ee3f8351b88b0a154a
AmanSonii <a.soni@eicare.org>
2021-03-26 16:39:18 +0530

    add initial code

---
+++ SwaggerIntegratedApp/Controllers/EmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SwaggerIntegratedApp.Model;
+
+namespace SwaggerIntegratedApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        List<Employee> employees;
+        public EmployeeController()
+        {
+           employees = new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                FirstName= "John",
+                LastName = "Smith",
+                EmailId ="John.Smith@gmail.com"
+            },
+            new Employee()
+            {
+                Id = 2,
+                FirstName= "Jane",
+                LastName = "Doe",
+                EmailId ="Jane.Doe@gmail.com"
+            }
+        };
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return employees;
+        }
+        // GET: api/Employee/5
+        [HttpGet("{id}", Name = "Get")]
+        public Employee Get(int id)
+        {
+            return employees.Find(e => e.Id == id);
+        }
+        // POST: api/Employee
+        [HttpPost]
+        [Produces("application/json")]
+        public Employee Post([FromBody] Employee employee)
+        {
+            // Logic to create new Employee
+            employees.Add(employee);
+            return employee;
+        }
+        // PUT: api/Employee/5
+        [HttpPut("{id}")]
+        public Employee Put(int id, [FromBody] Employee employee)
+        {
+            var updatedEmployee = employees.Find(e => e.Id == id);
+            if(updatedEmployee != null)
+            {
+                updatedEmployee = employee;
+                return updatedEmployee;
+            }
+            return null;
+        }
+        // DELETE: api/Employee/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            
+        }
+        private List<Employee> GetEmployees()
+        {
+            return new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                FirstName= "John",
+                LastName = "Smith",
+                EmailId ="John.Smith@gmail.com"
+            },
+            new Employee()
+            {
+                Id = 2,
+                FirstName= "Jane",
+                LastName = "Doe",
+                EmailId ="Jane.Doe@gmail.com"
+            }
+        };
+        }
+    }
+}
---
+++ SwaggerIntegratedApp/Model/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerIntegratedApp.Model
+{
+    public interface IEmployee
+    {
+         IEmployee GetEmployee();
+    }
+
+    public class Employee : IEmployee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+
+        public IEmployee GetEmployee()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
